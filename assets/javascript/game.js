21f38d08b868eb7d3eef7901216df78635bcc030
Craig Simmons <craig.a.simmons@gmail.com>
2019-03-22 13:59:39 -0700

    addded comments to code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ var playerScoreInt = 0; // Holds player's accumulated score during game
 var winInt = 0; // game wins counter
 var lossInt = 0; // game loss counter
 
-//-----------------------------------------------------
+/*-------- jQuery on.click function for 3 buttons at top of game ------------ */
 
 $("#start").on("click" , function () {
     startGame(); 
@@ -21,6 +21,8 @@ $("#reset").on("click" , function () {
 $("#again").on("click" , function () {
     playAgain(); 
 });
+
+/*-------- jQuery on.click function for each gem ------------ */
 
 $("#ruby").on("click" , function () {
     clickRuby();
@@ -38,11 +40,11 @@ $("#sapphire").on("click" , function () {
     clickSapphire();
 });
 
-//-----------------------------------------------------
+/*-------- Functions begin below ------------ */
 
-function startGame () {  // starts a game, resets several  variables, prints player score =0
-    diamondInt = 0;
-    rubyInt = 0;
+function startGame () {  // starts a game, resets gem value  variables, prints player score =0, generate random#'s
+    diamondInt = 0;      // linked to "Start" button. Does NOT reset wins/ losses, although this is easy to enable.
+    rubyInt = 0;       
     emeraldInt = 0;
     sapphireInt = 0;
     displayNumberInt = 0;
@@ -53,8 +55,8 @@ function startGame () {  // starts a game, resets several  variables, prints pla
     
 }
 
-function playAgain () {  
-    diamondInt = 0;
+function playAgain () {  // starts a game, resets several  variables, prints player score =0
+    diamondInt = 0;   // linked to "Play Again" button
     rubyInt = 0;
     emeraldInt = 0;
     sapphireInt = 0;
@@ -62,32 +64,34 @@ function playAgain () {
     playerScoreInt = 0;
     generateNumValues();
     $("#playerscore").text("  " + playerScoreInt);
-    $("#winloss").text("Select a Gem!");
+    $("#winloss").text("Play Again...");
 }
 
-function continueGame () { // runs during gem selection before a win/lose state occurs
-    $("#winloss").text("Select Again!");
+function continueGame () { // runs during gem selection BEFORE a win/lose state occurs. Nothing reset
+    $("#winloss").text("Select a Gem!");  // called automagically, not tied to a button
     $("#playerscore").text("  " + playerScoreInt); 
 }
 
-function winGame () {
+function winGame () { // called if player wins. increments winInt counter and updates screen
     displayNumberInt = 0;
     winInt = winInt + 1;
     $("#playerscore").text("  " + playerScoreInt); 
     $("#playerwins").text("  " + winInt);
     $("#winloss").text("You Win!");
+    // playAgain (); if we want game to queue up a new game automatically after a win/loss, add this line and remove button in HTML
 }
 
 
-function loseGame () {
+function loseGame () { // called if player loses a round. increments lossInt counter and updates screen
     displayNumberInt = 0;
     lossInt = lossInt + 1;
     $("#playerloss").text("  " + lossInt);
     $("#playerscore").text("  " + playerScoreInt); 
     $("#winloss").text("You Lose!");
+    // playAgain (); if we want game to queue up a new game automatically after a win/loss, add this line and remove button in HTML
 }
 
-function resetAll () {
+function resetAll () {  // self -explanatory: resets all vars and updates display fields.  Triggered by Reset button
     var diamondInt = 0; 
     var rubyInt = 0;
     var emeraldInt = 0;
@@ -102,7 +106,7 @@ function resetAll () {
     $("#displaynum").text("  " + displayNumberInt);
     $("#winloss").text("Click Start to Begin!");
 }
-
+// Generates random numbers and displayNumber that player must match to win
 // used Math.floor(Math.random() * (max# - min# + 1) + min# pattern to gennerate rand#s 
 function generateNumValues() {
     diamondInt = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
@@ -113,21 +117,21 @@ function generateNumValues() {
     $("#displaynum").text("  " + displayNumberInt);
 }
 
-function clickRuby () {
+function clickRuby () {  // called when ruby img is clicked. 
     playerScoreInt = playerScoreInt + rubyInt;
     
-    if (playerScoreInt === displayNumberInt) {
+    if (playerScoreInt === displayNumberInt) { // if game # = player score: Win!
         winGame();
     }
-    else if (playerScoreInt > displayNumberInt) {
+    else if (playerScoreInt > displayNumberInt) { // if player score > game Number: Lose!
         loseGame();
     }
-    else {
+    else {   // if player score is < game #: Continue with round.
         continueGame();
     }
 }
 
-function clickEmerald () {
+function clickEmerald () {  // See comments for clickRuby
     playerScoreInt = playerScoreInt + emeraldInt;
     
     if (playerScoreInt === displayNumberInt) {
@@ -141,7 +145,7 @@ function clickEmerald () {
     }
 }
 
-function clickDiamond () {
+function clickDiamond () {  // See comments for clickRuby
     playerScoreInt = playerScoreInt + diamondInt;
     
     if (playerScoreInt === displayNumberInt) {
@@ -155,7 +159,7 @@ function clickDiamond () {
     }
 }
 
-function clickSapphire () {
+function clickSapphire () {  // See comments for clickRuby
     playerScoreInt = playerScoreInt + sapphireInt;
     
     if (playerScoreInt === displayNumberInt) {
@@ -168,87 +172,3 @@ function clickSapphire () {
         continueGame();
     }
 }
-/*
-function clickEmerald () {
-    playerScoreInt = playerScoreInt + emeraldInt;
-
-    if (playerScoreInt === displayNumberInt) {
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("Winner!");
-        winInt = winInt + 1;
-        $("#playerwin").text("  " + winInt);
-        winGame();
-        // return winValuesArr = [winInt, playerScoreInt, winGame()];
-    }
-    else if (playerScoreInt > displayNumberInt) {
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("You Lose!");
-        lossInt = lossInt + 1;
-        $("#playerloss").text("  " + lossInt);
-        loseGame();
-        // return lossValuesArr = [lossInt, playerScoreInt, loseGame()];
-    }
-    else {
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("Select Again")
-        continueGame();
-        // return continueValues = [playerScoreInt, continueGame()];
-        
-    }
-}
-
-function clickDiamond () {
-    playerScoreInt = playerScoreInt + diamondInt;
-
-    if (playerScoreInt === displayNumberInt) {
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("Winner!");
-        winInt = winInt + 1;
-        $("#playerwin").text("  " + winInt);
-        winGame();
-        // return winValuesArr = [winInt, playerScoreInt, winGame()]
-    }
-    else if (playerScoreInt > displayNumberInt) {
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("You Lose!");
-        lossInt = lossInt + 1;
-        $("#playerloss").text("  " + lossInt);
-        loseGame();
-        // return lossValuesArr = [lossInt, playerScoreInt, loseGame()];
-    }
-    else {
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("Select Again")
-        continueGame()
-       // return continueValues = [playerScoreInt, continueGame()]
-    }
-}
-
-function clickSapphire () {
-    playerScoreInt = playerScoreInt + sapphireInt;
-
-    if (playerScoreInt === displayNumberInt) {
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("Winner!");
-        winInt = winInt + 1;
-        $("#playerwin").text("  " + winInt);
-        winGame();
-        // return winValuesArr = [winInt, playerScoreInt, winGame()]
-    }
-    else if (playerScoreInt > displayNumberInt) {
-        console.log("Sapphire playerloses")
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("You Lose!");
-        lossInt = lossInt + 1;
-        $("#playerloss").text("  " + lossInt);
-        loseGame();
-        // return lossValuesArr = [lossInt, playerScoreInt, loseGame()];
-    }
-    else {
-        $("#playerscore").text("  " + playerScoreInt); 
-        $("#winloss").text("Select Again")
-        continueGame()
-       // return continueValues = [playerScoreInt, continueGame()]
-    }
-}
-*/
