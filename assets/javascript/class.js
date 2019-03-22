65b68935a58a56f863a025699895c7c8d0de56e6
Craig Simmons <craig.a.simmons@gmail.com>
2019-03-21 21:44:54 -0700

    for deployment

--- assets/javascript/class.js
+++
@@ -1,8 +0,0 @@
-var sentenceStr = " ";
-var numWordsInt = 0;
-var wordBreakArr = []];
-
-var sentenceStr = input("Enter a sentence");
-wordBreakArr = str.split(" ")
-numWordsInt = wordBreakArr.length;
-alert("There are " + numWordsInt + " words in this sentence");
